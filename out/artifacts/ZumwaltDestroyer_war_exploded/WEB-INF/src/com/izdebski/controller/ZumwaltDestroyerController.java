4c83762dcffbfff511b794d8acf6ce83e91e8a67
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-01-17 20:09:02 +0200

    Spring MVC flow

--- out/artifacts/ZumwaltDestroyer_war_exploded/WEB-INF/src/com/izdebski/controller/ZumwaltDestroyerController.java
+++ out/artifacts/ZumwaltDestroyer_war_exploded/WEB-INF/src/com/izdebski/controller/ZumwaltDestroyerController.java
@@ -1,5 +1,7 @@
 package com.izdebski.controller;
 
+import com.izdebski.service.ZumwaltDestroyerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +11,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value="/")
 public class ZumwaltDestroyerController {
 
+    @Autowired
+    private ZumwaltDestroyerService zumwaltDestroyerService;
+
     @RequestMapping(method=RequestMethod.GET)
     public ModelAndView welcome(){
         return new ModelAndView("welcomePage", "welcomeMessage",
-                "Zumwalt-class guide missile destroyer!");
+                zumwaltDestroyerService.welcomeMessage());
+    }
+    @RequestMapping(value="welcomeagain" ,method=RequestMethod.GET)
+    public ModelAndView welcomeAgain(){
+        return new ModelAndView("welcomePage", "welcomeMessage",
+                zumwaltDestroyerService.welcomeAgainMessage());
     }
 }
---
+++ src/com/izdebski/service/ZumwaltDestroyerService.java
@@ -0,0 +1,15 @@
+package com.izdebski.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ZumwaltDestroyerService {
+
+    public String welcomeMessage(){
+        return "Zumwalt-class guide missile destroyer!";
+    }
+
+    public Object welcomeAgainMessage(){
+        return "Zumwalt-class another guide missile destroyer!";
+    }
+}
